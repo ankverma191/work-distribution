d9d03034a4199d8158dd1d0298fb2eab2deddca9
ankit verma <vermaankit84@gmail.com>
2020-02-04 01:43:02 -0700

    db integration

---
+++ src/test/java/com/freedom/financial/work/distribution/service/engine/engines/TaskRuleEngine.java
@@ -0,0 +1,8 @@
+package com.freedom.financial.work.distribution.service.engine.engines;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TaskRuleEngine {
+}
